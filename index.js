6ee2cb0ea798738d91b9c418130e29a410939ca4
Shyaboi <ian.shane.sears@gmail.com>
2020-06-28 22:52:38 -0700

    added button click alert and clg

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+
+document.getElementById("heroButt").onclick = function() {click()};
+document.getElementById("momButt").onclick = function() {click()};
+document.getElementById("kidButt").onclick = function() {click()};
+
+
 function click() {
-    alert('you have clicked a thing')
+    alert('check console log')
+    console.log('You Clicked A Button')
 }
